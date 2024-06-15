e5b58df47728ee55cd44350ed4d791e8ce2b3c1c
yuun-dev <yuun.dev@gmail.com>
2024-06-15 12:52:37 +0900

    first commit

---
+++ src/main/java/com/example/chattingroom2/ChattingRoom2Application.java
@@ -0,0 +1,13 @@
+package com.example.chattingroom2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingRoom2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingRoom2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chattingroom2/ChattingRoom2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chattingroom2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingRoom2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
