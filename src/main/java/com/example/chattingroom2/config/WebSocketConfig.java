3837c91bc5de2d1eba0b1c3136ee74a6d1fe7a3f
yuun-dev <yuun.dev@gmail.com>
2024-06-19 16:49:21 +0900

    revert

--- src/main/java/com/example/chattingroom2/config/WebSocketConfig.java
+++ src/main/java/com/example/chattingroom2/config/WebSocketConfig.java
@@ -26,9 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws-stomp")// 엔드포인트: ws://ip:port/ws-stomp
-//                .setAllowedOrigins("*")
-                .setAllowedOriginPatterns("*");
-//                .withSockJS();
+                .setAllowedOrigins("*");
     }
 
     @Override
