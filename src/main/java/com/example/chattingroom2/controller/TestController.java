247790aeecfddec9bbb13c2780f1aa1a8cd10a08
yuun-dev <yuun.dev@gmail.com>
2024-06-29 12:49:33 +0900

    refactor:refactoring

--- src/main/java/com/example/chattingroom2/controller/TestController.java
+++ src/main/java/com/example/chattingroom2/controller/TestController.java
@@ -1,17 +1,12 @@
 package com.example.chattingroom2.controller;
 
-import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
-
-    @Resource(name = "redisTemplate")
-    private ValueOperations<String, String> valueOperation;
 
     @GetMapping("/")
     public String hello() {
@@ -21,17 +16,5 @@ public class TestController {
     @GetMapping("/success")
     public String loginSuccess() {
         return "login success~!";
-    }
-
-    @PostMapping("/redis") // for test
-    public String redisInsertTest(@RequestParam String key, @RequestParam String value) {
-        valueOperation.set(key, value);
-        return "insert success";
-    }
-
-    @GetMapping("/redis/{key}") // for test
-    public ResponseEntity<String> redisGetTest(@PathVariable String key) {
-        String value = valueOperation.get(key);
-        return ResponseEntity.ok(key + " : " + value);
     }
 }
