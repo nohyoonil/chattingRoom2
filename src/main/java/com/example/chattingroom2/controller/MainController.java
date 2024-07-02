e7e79479c0ace280273e114472a35abfe6dda111
yuun-dev <yuun.dev@gmail.com>
2024-07-02 13:40:30 +0900

    refactor: controller 이름 수정

--- src/main/java/com/example/chattingroom2/controller/MainController.java
+++ src/main/java/com/example/chattingroom2/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class MainController {
 
     @GetMapping("/")
     public String hello() {
